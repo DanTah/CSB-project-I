69574cc8eb73382c4a40eadeba74a40cf54e8c60
DanTah <luca.tahtela@gmail.com>
2025-10-05 17:51:57 +0300

    Restructure login and register

--- users.py
+++ users.py
@@ -1,5 +1,5 @@
 import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
 def get_user(user_id):
     sql = """SELECT id, username
              FROM users
@@ -14,3 +14,19 @@ def get_recipes(user_id):
              WHERE user_id = ?
              ORDER BY id"""
     return db.query(sql,[user_id])
+
+def create_user(username, password):
+    password_hash = generate_password_hash(password)
+    sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+    db.execute(sql, [username, password_hash])
+
+def check_login(username, password):
+    sql = "SELECT id, password_hash FROM users WHERE username = ?"
+    result = db.query(sql, [username])[0]
+    if not result:
+        return None
+    user_id = result["id"]
+    password_hash = result["password_hash"]
+    if check_password_hash(password_hash, password):
+        return user_id
+    return None
