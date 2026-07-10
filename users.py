8884e2671ff62a63143c26d924e49a90253e2e83
DanTah <luca.tahtela@gmail.com>
2026-07-10 20:00:03 +0300

    Add password hash flaw

--- users.py
+++ users.py
@@ -28,9 +28,11 @@ def get_recipes(user_id):
     return db.query(sql, [user_id])
 
 def create_user(username, password):
-    password_hash = generate_password_hash(password)
     sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-    db.execute(sql, [username, password_hash])
+    db.execute(sql, [username, password])
+    #password_hash = generate_password_hash(password)
+    #sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+    #db.execute(sql, [username, password_hash])
 
 def check_login(username, password):
     sql = """SELECT id, password_hash
@@ -42,6 +44,8 @@ def check_login(username, password):
     result = result[0]
     user_id = result["id"]
     password_hash = result["password_hash"]
-    if check_password_hash(password_hash, password):
+    if password_hash == password:
         return user_id
+    #if check_password_hash(password_hash, password):
+    #    return user_id
     return None
